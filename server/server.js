90c78555ba4a4dcc1b9fcc98d69cba9333c9773c
Exploration <zhangphil@outlook.com>
2024-03-13 06:33:16 +0000

    Mar 12, 2024, 11:33 PM

--- server/server.js
+++ server/server.js
@@ -5,7 +5,6 @@ let msgpack = require("@msgpack/msgpack");
 let server = dgram.createSocket("udp4");
 
 server.on("message", function(msg, rinfo){
-   
     console.log(msgpack.decode(msg));
     server.send("message recieved", rinfo.port, rinfo.address); 
 });
