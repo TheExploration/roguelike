b190092e24418147dc123a4030a27659b6a94049
Exploration <zhangphil@outlook.com>
2024-03-17 21:39:31 +0000

    Mar 17, 2024, 2:39 PM

--- server/server.js
+++ server/server.js
@@ -21,24 +21,7 @@ const DATATYPE = {
 let data;
 
 function server_assign_id(){
-
-    var pid = -1;
-    if (ds_list_find_index(player_ids, 1) == -1) {
-        ds_list_add(player_ids, 1);
-        pid = 1;
-    } else if (ds_list_find_index(player_ids, 2) == -1) {
-        ds_list_add(player_ids, 2);
-        pid = 2;
-    } else if (ds_list_find_index(player_ids, 3) == -1) {
-        ds_list_add(player_ids, 3);
-        pid = 3;
-    } else if (ds_list_find_index(player_ids, 4) == -1) {
-        ds_list_add(player_ids, 4);
-        pid = 4;
-    } else {
-        //server full
-    }
-    return pid;
+    
     
     
 }
