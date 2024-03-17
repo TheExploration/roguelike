d1fec6a43a6f41ffe4a9167c9d85bee950a3f5d8
Exploration <zhangphil@outlook.com>
2024-03-17 07:19:39 +0000

    Mar 17, 2024, 12:19 AM

--- server/server.js
+++ server/server.js
@@ -19,6 +19,6 @@ const data = {
 };
 
 server.on("message", function(msg, rinfo){
-    console.log(decode(msg));
+    console.log(messagepack.decode(msg));
     server.send("message recieved", rinfo.port, rinfo.address); 
 });
